940ba2f032ff71268f06402263a87f58866be7a9
teacherIan <ian_malloy@ssis.asia>
2023-07-20 23:09:03 +0800

    Initial Commit

---
+++ MalloyGolf/MalloyGolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalloyGolfApp.swift
+//  MalloyGolf
+//
+//  Created by Ian Malloy on 2023/7/20.
+//
+
+import SwiftUI
+
+@main
+struct MalloyGolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
